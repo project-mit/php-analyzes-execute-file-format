d437e6e8286e827257846a5ec551de95853f2091
pro_hacker <quddnr145@naver.com>
2014-08-17 06:10:45 +0900

    add IMAGE_DOS_HEADER structure and IMAGE_NT_HEADERS structure of 32 bit

---
+++ ExecuteFormat/PE/PE32ImageDosHeader.php
@@ -0,0 +1,26 @@
+<?php
+namespace PHPSecurityUploader\ExecuteFormat\PE\32;
+
+class ImageDosHeader
+{
+    public $magic;
+    public $cblp;
+    public $cp;
+    public $crlc;
+    public $cparhdr;
+    public $minalloc;
+    public $maxalloc;
+    public $ss;
+    public $sp;
+    public $csum;
+    public $ip;
+    public $cs;
+    public $lfarlc;
+    public $ovno;
+    public $reservd1 = [0, 0, 0, 0];
+    public $oemid;
+    public $oeminfo;
+    public $reservd2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    public $lfanew;
+}
+?>
---
+++ ExecuteFormat/PE/PE32ImageNtHeaders.php
@@ -0,0 +1,13 @@
+<?php
+namespace PHPSecurityUploader\ExecuteFormat\PE\32;
+
+class ImageNtHeaders
+{
+    public $signature;
+
+    // need to modify
+    public $fileheader;
+    // need to modify
+    public $optionalheader;
+}
+?>
