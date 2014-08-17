b956f5b94c6768d3c80d70f0fb1647cc352c7551
pro_hacker <quddnr145@naver.com>
2014-08-17 19:55:09 +0900

    add IMAGE_DATA_DIRECTORY and modify IMAGE_NT_HEADERS

---
+++ ExecuteFormat/PE/PE32ImageDataDirectory.php
@@ -0,0 +1,25 @@
+<?php
+namespace PHPSecurityUploader\ExecuteFormat\PE\32;
+
+class ImageDataDirectory
+{
+    const EXPORT_DIRECTORY = 0x0;
+    const IMPORT_DIRECTORY = 0x1;
+    const RESOURCE_DIRECTORY = 0x2;
+    const EXCEPTION_DIRECTORY = 0x3;
+    const SECURITY_DIRECTORY = 0x4;
+    const BASERELOC_DIRECTORY = 0x5;
+    const DEBUG_DIRECTORY = 0x6;
+    const COPYRIGHT_DIRECTORY = 0x7;
+    const GLOBALPTR_DIRECTORY = 0x8;
+    const TLS_DIRECTORY = 0x9;
+    const LOAD_CONFIG_DIRECTORY = 0xA;
+    const BOUND_IMPORT_DIRECTORY = 0xB;
+    const IAT_DIRECTORY = 0xC;
+    const DELAY_IMPORT_DIRECTORY = 0xD;
+    const COM_DESCRIPTOR_DIRECTORY = 0xE;
+    const RESERVED_DIRECTORY = 0xF;
+
+    public $virtualAddress;
+    public $size;
+}
--- ExecuteFormat/PE/PE32ImageNtHeaders.php
+++ ExecuteFormat/PE/PE32ImageNtHeaders.php
@@ -3,11 +3,50 @@ namespace PHPSecurityUploader\ExecuteFormat\PE\32;
 
 class ImageNtHeaders
 {
+    const IMAGE_NUMBEROF_DIRECTORY_ENTRIES = 16;
+
     public $signature;
 
-    // need to modify
-    public $fileheader;
-    // need to modify
-    public $optionalheader;
+    // IMAGE_FILE_HEADER
+    public $machine;
+    public $numberOfSections;
+    public $timeDateStamp;
+    public $pointerToSymbolTable;
+    public $numberOfSymbols;
+    public $sizeOfOptionalHeader;
+    public $characteristics;
+
+    // IMAGE_OPTIONAL_HEADER
+    public $magic;
+    public $majorLinkerVersion;
+    public $minorLinkerVersion;
+    public $sizeOfCode;
+    public $sizeOfInitializedData;
+    public $sizeOfUninitializedData;
+    public $addressOfEntryPoint;
+    public $baseOfCode;
+    public $baseOfData;
+    public $imageBase;
+    public $sectionAlignment;
+    public $fileAlignment;
+    public $majorOperatingSystemVersion;
+    public $minorOperatingSystemVersion;
+    public $majorImageVersion;
+    public $minorImageVersion;
+    public $majorSubsystemVersion;
+    public $minorSubsystemVersion;
+    public $win32VersionValue;
+    public $sizeOfImage;
+    public $sizeOfHeaders;
+    public $checksum;
+    public $subsystem;
+    public $dllCharacteristics;
+    public $sizeOfStackReserve;
+    public $sizeOfStackCommit;
+    public $sizeOfHeapReserve;
+    public $sizeOfHeapCommit;
+    public $loaderFlags;
+    public $numberOfRvaAndSizes;
+    public $dataDirectory = array_fill(0, self::IMAGE_NUMBEROF_DIRECTORY_ENTRIES, new ImageDataDirectory());
 }
 ?>
