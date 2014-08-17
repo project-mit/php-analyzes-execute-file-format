1fbf7dadc26b4973f3e3633301236aeada9dae3f
pro_hacker <quddnr145@naver.com>
2014-08-17 22:00:14 +0900

    add IMAGE_SECTION_HEADER structure

---
+++ ExecuteFormat/PE/PE32ImageSectionHeader.php
@@ -0,0 +1,35 @@
+<?php
+namespace AnalyzesExecuteFileFormat\ExecuteFormat\PE\32;
+
+class ImageSectionHeader
+{
+    const IMAGE_SIZEOF_SHORT_NAME = 8;
+
+    public $name = null;
+    public $misc = [
+        'physicalAddress' => 0,
+        'virtualSize' => 0
+    ];
+    public $virtualAddress;
+    public $sizeOfRawData;
+    public $pointerToRawData;
+    public $pointerToLineNumbers;
+    public $numberOfRelocations;
+    public $numberOfLineNumbers;
+    public $ccharacteristics;
+
+    public function __construct()
+    {
+        $this->name = array_fill(0, self::IMAGE_SIZEOF_SHORT_NAME, '');
+    }
+
+    public function __destruct()
+    {
+        foreach ($this->name as $key => $value)
+        {
+            unset($this->name[$key]);
+        }
+        unset($this->name);
+    }
+}
+?>
